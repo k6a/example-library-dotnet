fd3cb2a494eeeed63271a773c2c0c392b5d9fa46
k6a <daisuke.kanazawa@gmail.com>
2023-10-01 20:08:52 +0900

    init

---
+++ src/K6a.Example.Model/FooRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace K6a.Example
+{
+    /// <summary>
+    /// Foo request
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class FooRequest
+    {
+        /// <summary>
+        /// Name to register.
+        /// </summary>
+        [DataMember(EmitDefaultValue = false, IsRequired = true, Name = "name", Order = 0)]
+        [JsonPropertyName("name"), JsonPropertyOrder(0)]
+        public string? Name { get; set; }
+    }
+}
---
+++ src/K6a.Example.Model/FooResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace K6a.Example.Model
+{
+    /// <summary>
+    /// Response for Foo
+    /// </summary>
+    [DataContract]
+    [Serializable]
+    public class FooResponse
+    {
+        /// <summary>
+        /// Response status code
+        /// </summary>
+        [DataMember(EmitDefaultValue = false, IsRequired = true, Name = "responseCode", Order = 0)]
+        [JsonPropertyName("responseCode"), JsonPropertyOrder(0)]
+        public int? ResponseCode { get; set; }
+    }
+}
